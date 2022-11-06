801ab868c2e472a238e2183aa22ff9dba29692f9
Janith Malshan <janithmalshan@gmail.com>
2022-11-06 18:33:42 +0200

    filter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import TableCountries from "./components/TableCountries";
 
 function App() {
   return (
     <div className="App">
-      <></>
+      <>
+        <h2>Country Code filter component</h2>
+        <TableCountries />
+      </>
     </div>
   );
 }
---
+++ src/components/TableCountries.tsx
@@ -0,0 +1,58 @@
+import {useState} from "react";
+import {ApolloClient, InMemoryCache, gql, useQuery} from '@apollo/client';
+import "./TableCountris.css"
+
+// initialize a GraphQL client
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'https://countries.trevorblades.com'
+});
+
+// GraphQL query that asks for names and codes for all countries
+const LIST_COUNTRIES = gql`
+  {
+    countries {
+      name
+      code
+    }
+  }
+`;
+
+interface Country {
+    name: string;
+    code: string;
+}
+
+function TableCountries() {
+    const {data, loading, error} = useQuery(LIST_COUNTRIES, {client});
+    const [filterText, setFilterText] = useState("");
+
+    if (loading || error) {
+        return <p>{error ? error.message : 'Loading...'}</p>;
+    }
+    return (
+        <>
+            <table className='ui-table'>
+                <tr>
+                    <th>Country Name</th>
+                    <th>Country Code <input className='ui-table__input' placeholder='Filter' maxLength={2} onChange={(event => {
+                        setFilterText(event.target.value)
+                    })}/></th>
+                </tr>
+                {data.countries.filter((country:Country) => {
+                    if (filterText == "") {
+                        return country
+                    } else if (country.code.toLowerCase().includes(filterText.toLowerCase())) {
+                        return country
+                    }
+                }).map((country:Country) => (
+                    <tr key={country.code}>
+                        <td>{country.name}</td><td>{country.code}</td>
+                    </tr>
+                ))}
+            </table>
+        </>
+    )
+}
+
+export default TableCountries
